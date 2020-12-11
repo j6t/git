c480778452427134d8a54068d04f809aee136cce
Johannes Sixt <j6t@kdbg.org>
2020-12-11 18:00:49 +0100

    Today's build.

--- rebuild.sh
+++ rebuild.sh
@@ -74,22 +74,6 @@ merge misc-patches
 pick t3903-stash-racily-clean
 
 # these are completed:
-#pull origin dl/difftool-mergetool
-#pull origin dl/rebase-i-keep-base
-#pull origin bc/reread-attributes-during-rebase
-#pull origin bw/rebase-autostash-keep-current-branch
-#pull origin nd/diff-parseopt
-#pull origin pw/rebase-i-show-HEAD-to-reword
-#pull origin tg/t0021-racefix
-#pull origin js/visual-studio
-#pull origin js/gitdir-at-unc-root
-#pull origin ar/mingw-run-external-with-non-ascii-path
-#pull origin sb/userdiff-dts
-#pull origin tb/file-url-to-unc-path
-#pull origin tg/t0021-racefix
-#pull origin sg/progress-fix
-#pull origin dl/octopus-graph-bug
-#pull origin js/diff-rename-force-stable-sort
 #pull -s subtree pratyush py/call-do-quit-before-exit
 #pull -s subtree pratyush bw/commit-scrollbuffer
 #pull -s subtree pratyush bp/widget-focus-hotkeys
@@ -102,22 +86,16 @@ pick t3903-stash-racily-clean
 pick t5580-lower-case-drive
 pick t7500-in-dir-w-space
 merge git-post
-pull origin mk/use-size-t-in-zlib
+# pull origin mk/use-size-t-in-zlib
 
 # this needs:
 # git remote add pratyush https://github.com/prati0100/git-gui.git
-merge -s subtree git-gui-revert-bw-revert-hunk
-pull -s subtree pratyush master
+# merge -s subtree git-gui-revert-bw-revert-hunk
+# pull -s subtree pratyush master
 pull -s subtree pratyush py/reload-config
-pull -s subtree pratyush js/hooks-path
 pull -s subtree pratyush bp/select-staged-on-commit-focus
-#pull https://github.com/gitgitgadget/git pr-480/dscho/mingw-inherit-only-std-handles-set-errno-v2
-pull origin ag/rebase-avoid-unneeded-checkout
-pull origin ds/graph-horizontal-edges
+pull -s subtree pratyush sh/auto-rescan
 pick snprintf-keep-errno
-pull origin js/mingw-fixes
-pull origin js/mingw-is-hidden-test-fix
-pull origin js/mingw-isilon-nfs
-pull origin js/rebase-autosquash-double-fixup-fix
+#pick mingw-poll-writable-fix
 pick skip-failing-tests
 #pick t3008-path-lookup
