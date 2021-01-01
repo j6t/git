ce8ca051f14853e8584d4681c84096ea1a5edff9
Johannes Sixt <j6t@kdbg.org>
2021-01-01 11:25:47 +0100

    mingw.c: fix alternate screen using TERM=xterm-256color
    With a recent MinGW64 update, the terminal does not switch between main
    screen buffer and alternate screen buffer correctly when TERM is set to
    "cygwin". In particular, after less and vim end, the command prompt is
    reset to the top of the terminal window and old text remains on the
    screen.
    
    My tests show that TERM=xterm-256color works as intended.
    
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>

--- compat/mingw.c
+++ compat/mingw.c
@@ -2641,7 +2641,7 @@ static void setup_windows_environment(void)
 
 	/* simulate TERM to enable auto-color (see color.c) */
 	if (!getenv("TERM"))
-		setenv("TERM", "cygwin", 1);
+		setenv("TERM", "xterm-256color", 1);
 
 	/* calculate HOME if not set */
 	if (!getenv("HOME")) {
