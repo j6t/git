fed3ef27f33a75384054d4d1e99d28ab4e4e94b1
Johannes Sixt <j6t@kdbg.org>
2021-02-04 10:01:51 +0100

    Today's build.

--- rebuild.sh
+++ rebuild.sh
@@ -97,6 +97,8 @@ pull -s subtree pratyush bp/select-staged-on-commit-focus
 pull -s subtree pratyush sh/auto-rescan
 pick snprintf-keep-errno
 #pick mingw-poll-writable-fix
+pull origin pb/blame-funcname-range-userdiff
+pull origin ab/pager-exit-log
 pick fix-implicit-localtime_r
 pick skip-failing-tests
 #pick t3008-path-lookup
